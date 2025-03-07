05c68dc67596d99f5c1a66180a0c938fe48e902f
Steven Christiano <stevenc1707@gmail.com>
2025-03-07 10:28:29 +0700

    Menyimpan perubahan sebelum pull

--- backend/StevenAccelokaAPI/obj/Debug/net8.0/StevenAccelokaAPI.AssemblyInfo.cs
+++ backend/StevenAccelokaAPI/obj/Debug/net8.0/StevenAccelokaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StevenAccelokaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326ae1e444ad4e425687ac4317ec813396b0977a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StevenAccelokaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StevenAccelokaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
